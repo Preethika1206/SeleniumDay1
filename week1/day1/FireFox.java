558c9d3acf1d8232035084b638effa1ff25180c7
Preethika1206 <preethika12061989@gmail.com>
2024-12-16 23:59:56 +0530

    Add files via upload
    Da1

---
+++ week1/day1/FireFox.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class FireFox {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int releaseYear = 1998;
+		float browserVersion = 100.2f;
+		String browserName ="FireFox";
+		Boolean isVisible = true;
+		char browserLogo = 'f';
+
+		System.out.println("Name of the Browser :"+browserName +"\n"+"Dispalyed BrowserLogo :"+browserLogo +"\n"+"FireFox Release Year :"+releaseYear +"\n" +"FireFox browser version:"+browserVersion+"\n" +"FireFox Application is visible : "+ isVisible);
+
+	}
+
+}
